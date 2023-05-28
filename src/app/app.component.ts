4bde9aa1588310175ab1bad36143d34e0c487f84
draic9 <kuba.samosiuk@gmail.com>
2023-05-28 20:24:33 +0200

    Deploying to GitHub Pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  listItems: string[] = [];
+  secondListItems: string[] = [];
+
+  addItem() {
+    // Logic to handle form submission and add the item to the listItems array
+    const firstNameInput = (document.getElementById('firstName') as HTMLInputElement);
+    const lastNameInput = (document.getElementById('lastName') as HTMLInputElement);
+
+    const firstName = firstNameInput.value.trim();
+    const lastName = lastNameInput.value.trim();
+    // const fullName = firstName + ' ' + lastName;
+    // this.listItems.push(fullName);
+
+    // Check if the submission is not empty
+    if (firstName !== '' && lastName !== '') {
+      const fullName = firstName + ' ' + lastName;
+      this.listItems.push(fullName)
+    }
+
+    // Clear the form fields
+    (document.getElementById('firstName') as HTMLInputElement).value = '';
+    (document.getElementById('lastName') as HTMLInputElement).value = '';
+  }
   title = 'name-bulletin';
+
+  deleteItem(index: number) {
+    this.listItems.splice(index, 1);
+  }
+
+  moveItem(index: number) {
+    const itemToMove = this.listItems[index];
+    this.secondListItems.push(itemToMove);
+    this.deleteItem(index);
+  }
 }
+
+
